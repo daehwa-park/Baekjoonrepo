3848a59c15c9ed60c0f15f06ffa9a8c1df72b6e6
daehwa-park <71697397+daehwa-park@users.noreply.github.com>
2023-04-05 10:02:27 +0900

    [Gold IV] Title: 회전 초밥, Time: 552 ms, Memory: 170972 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        
+		int n = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		int[] sushi = new int[n];
+		for (int i = 0; i < n; i++) {
+			sushi[i] = Integer.parseInt(br.readLine());
+		}
+
+		// end input
+		
+		int max = -1;
+		int cnt = 0;
+		int[] visited = new int[d + 1];
+		boolean free = false;
+
+		for (int i = 0; i < k; i++) {
+			if (visited[sushi[i]] == 0) {
+				cnt++;
+			}
+			visited[sushi[i]]++;
+		}
+		if (visited[c] == 0) {
+			free = true;
+		}
+		if (free) {
+			max = Math.max(max, cnt + 1);
+		} else {
+			max = Math.max(max, cnt);
+		}
+
+
+		int sp = 0;
+		int ep = k - 1;
+		while(sp != n) {
+			sp++;
+			ep = (ep + 1) % n;
+			visited[sushi[sp - 1]]--;
+			if (visited[sushi[sp - 1]] == 0) {
+				cnt--;
+			}
+			if (visited[sushi[ep]] == 0) {
+				cnt++;
+			}
+			visited[sushi[ep]]++;
+
+			if (visited[c] != 0) {
+				free = false;
+			} else {
+				free = true;
+			}
+
+			if (free) {
+				max = Math.max(max, cnt + 1);
+			} else {
+				max = Math.max(max, cnt);
+			}
+		}
+
+		System.out.println(max);
+        
+    }
+}
