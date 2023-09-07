50a261ffcc933180c190585a8a410d7f3d214858
daehwa-park <71697397+daehwa-park@users.noreply.github.com>
2023-09-07 22:31:58 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -4,36 +4,17 @@ class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
         
-        int count = 1; // 몇번째 차례인지
-        int num = 2; // 번호
-        
         Set<String> set = new HashSet<>();
         set.add(words[0]); //첫번째는 무조건 성공
         
         for(int i = 1; i < words.length; i++) {
-            if(words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)){
-                answer[0] = num;
-                answer[1] = count;
+            if(words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0) || set.contains(words[i]) || words[i].length() == 1 ){
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
                 break;
             }
-            else if(set.contains(words[i])) {
-                answer[0] = num;
-                answer[1] = count;
-                break;
-            }
-            else if(words[i].length() == 1) {
-                answer[0] = num;
-                answer[1] = count;
-                break;
-            }
-            
-            num++;
+
             set.add(words[i]);
-            
-            if(num == n + 1) {
-                num = 1;
-                count++;
-            }
             
         }
         
