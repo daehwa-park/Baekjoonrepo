dd50aa2a35ea5310a01682ccab1c2c4aa2d1c34b
daehwa-park <71697397+daehwa-park@users.noreply.github.com>
2023-12-13 21:49:30 +0900

    [Silver I] Title: 경로 찾기, Time: 4092 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,32 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+
+def dfs(start, dest):
+   global is_route
+   for i in range(n):
+      if array[start][i] == 1 and not visited[i]:
+         if i == dest:
+            is_route = True
+         else:
+            visited[i] = True
+            dfs(i, dest)
+
+n = int(sys.stdin.readline())
+
+array = []
+for _ in range(n):
+   array.append(list(map(int, sys.stdin.readline().split())))
+
+answer = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+   for j in range(n):
+      visited = [False for _ in range(n)]
+      is_route = False
+      dfs(i, j)
+      if is_route:
+         answer[i][j] = 1
+
+for i in range(n):
+   print(" ".join(map(str, answer[i])))
