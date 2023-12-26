85920c4a99bb016ed356d02230990c4a4d0fffd3
daehwa-park <71697397+daehwa-park@users.noreply.github.com>
2023-12-26 23:34:14 +0900

    [level 2] Title: n^2 배열 자르기, Time: 62.60 ms, Memory: 17.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -0,0 +1,12 @@
+def solution(n, left, right):
+    answer = []
+    
+    for i in range(left, right + 1):
+        q = i // n # 몫
+        d = i % n # 나머지
+        
+        num = max(q + 1, d + 1)
+        answer.append(num)
+        
+    
+    return answer
