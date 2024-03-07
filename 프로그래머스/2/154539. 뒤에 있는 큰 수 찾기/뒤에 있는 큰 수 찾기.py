b04796fdfbb3d7585bd27db77077673da9d1f98a
daehwa-park <71697397+daehwa-park@users.noreply.github.com>
2024-03-07 23:46:19 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 162.23 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -4,13 +4,9 @@ def solution(numbers):
     answer = [-1] * len(numbers)
     s = deque()
     
-    for i, n in enumerate(numbers):
-        while True:
-            if not s or s[-1][0] >= n:
-                s.append([n, i])
-                break
-            else:
-                tn, ti = s.pop()
-                answer[ti] = n
+    for i in range(len(numbers)):
+        while s and numbers[s[-1]] < numbers[i]:
+            answer[s.pop()] = numbers[i]   
+        s.append(i)
     
     return answer
