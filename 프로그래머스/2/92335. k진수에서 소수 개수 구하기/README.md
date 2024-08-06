c424a71494c2cea50bbc9139cdcda65342ab46ea
daehwa-park <71697397+daehwa-park@users.noreply.github.com>
2024-08-06 18:58:34 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 24.23 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.03 ms
+메모리: 81.8 MB, 시간: 24.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 23:2:15
+2024년 08월 06일 18:58:33
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String s = "";
+        
+        while(n / k != 0 || n % k != 0) {
+            s = (n % k) + s;
+            n /= k;
+        }
+        // 진수 변환
+        
+        System.out.print(s);
+        
+        StringTokenizer st = new StringTokenizer(s, "0");
+        while(st.hasMoreTokens()) {
+            if(isPrime(Long.parseLong(st.nextToken()))) {
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+    public boolean isPrime(long num) {
+        boolean p = true;
+        
+        if (num == 1) {
+            return false;
+        }
+        for(int i = 2; i < (long)Math.sqrt(num) + 1 ; i++) {
+            if (num % i == 0) {
+                p = false;
+                break;
+            }
+        }
+        return p;
+    }
+}
